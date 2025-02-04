10730f9cac5a54f7de2f7d8b992b72422da79505
GuiSmith <guilhermessmith2014@gmail.com>
2025-02-04 07:37:19 -0300

    método OPTIONS adicionado

--- back/api/config.php
+++ back/api/config.php
@@ -3,8 +3,13 @@
     // Configurar cabeçalhos HTTP
     header("Content-Type: application/json");
     header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        http_response_code(200);
+        exit();
+    }
 
     // Método não permitido
     function methodNotAllowed(): array
